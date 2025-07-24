f6993d936b5d258cc0c8175db5ca6692b5174235
israaellban <israaellban@gmail.com>
2025-07-24 20:46:24 +0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753379181942

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5790b71e._.js",
+  "static/chunks/node_modules_1a9c7de4._.js"
+],
+    source: "dynamic"
+});
